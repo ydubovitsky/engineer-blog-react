43ba7489066b679dfd2a1e561c7c73027a77c6d9
user <y.dubovitsky@gmail.com>
2021-12-21 14:21:42 +0300

    added article component and routing between the list of posts and the selected post

--- src/components/main/content/content.component.jsx
+++ src/components/main/content/content.component.jsx
@@ -4,26 +4,38 @@ import AboutMe from '../about-me/about-me.component';
 import FollowMe from '../follow-me/follow-me.component';
 import Subscribe from '../subscribe/subscribe.component';
 import Search from '../search/search.component';
+import Article from '../../article/article.component';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const Content = (props) => {
   return (
-    <div className={styles.container}>
-      <PostList />
-      <div className={styles.aside}>
-        <div className={styles.about}>
-          <AboutMe />
-        </div>
-        <div className={styles.follow}>
-          <FollowMe />
-        </div>
-        <div className={styles.subscribe}>
-          <Subscribe />
-        </div>
-        <div className={styles.search}>
-          <Search />
+    <Router>
+      <div className={styles.container}>
+        <Routes >
+          <Route path="/" element={<PostList />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+        <div className={styles.aside}>
+          <div className={styles.about}>
+            <AboutMe />
+          </div>
+          <div className={styles.follow}>
+            <FollowMe />
+          </div>
+          <div className={styles.subscribe}>
+            <Subscribe />
+          </div>
+          <div className={styles.search}>
+            <Search />
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   )
 };
 
