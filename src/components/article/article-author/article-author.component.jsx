792acb9d8e1fdded74c81a54663cefe5ec2e13b8
user <y.dubovitsky@gmail.com>
2022-01-20 17:22:24 +0300

    added article component

---
+++ src/components/article/article-author/article-author.component.jsx
@@ -0,0 +1,19 @@
+import styles from './article-author.module.css';
+
+const ArticleAuthor = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.avatar}>
+        <img src="https://secure.gravatar.com/avatar/a1e1fa4e9559f47fe8abcb0f1df4357c?s=140&d=mm&r=g" alt="" srcset="" />
+      </div>
+      <div className={styles.author}>
+        <div className={styles.name}>Matthey Flowers</div>
+        <div className={styles.info}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti similique odit dicta, sapiente accusantium enim dolore voluptas reiciendis sequi alias saepe molestias debitis quam laboriosam incidunt vel nisi laudantium voluptatum?
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ArticleAuthor;
