8defc3f62209034088f06d06b8171572a58cc2e8
user <y.dubovitsky@gmail.com>
2022-01-26 16:15:37 +0300

    The sidebar is now a managed component

--- src/components/common/sidebar/sidebar.component.jsx
+++ src/components/common/sidebar/sidebar.component.jsx
@@ -1,13 +1,32 @@
 import cn from 'classnames';
 import { useRef } from 'react';
 import styles from './sidebar.module.css';
+import { Link } from 'react-router-dom';
+import { authSelector, logout } from '../../../redux/features/auth/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Sidebar = () => {
 
+  const dispatch = useDispatch();
   const sidebarRef = useRef(null);
+  const { authEntity } = useSelector(authSelector);
 
   const toggleSidebar = () => {
     sidebarRef.current.classList.toggle(styles.isOpen);
+  }
+
+  const showMenu = (user) => {
+    if (user) {
+      return <>
+        <div className={styles}>Hello, {user}</div>
+        <Link to={"/dashboard"}>Dashboard</Link>
+        <div onClick={() => dispatch(logout())}>Logout</div>
+      </>
+    }
+    return <>
+      <h4>Please Login!</h4>
+      <Link to={"/login"}>Login!</Link>
+    </>
   }
 
   return (
@@ -16,7 +35,7 @@ const Sidebar = () => {
         <i className={cn("fas", "fa-cog", styles.icon)}></i>
       </div>
       <div className={styles.menu}>
-
+        {showMenu(authEntity.username)}
       </div>
     </div>
   )
