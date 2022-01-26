f197f7d36aa637cd5d940cdf62bc58942cb7997c
user <y.dubovitsky@gmail.com>
2022-01-26 15:29:21 +0300

    added authSlice for the authentication

---
+++ src/redux/features/auth/authSlice.js
@@ -0,0 +1,62 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import callApi from '../../requests/callApi';
+
+// ------------------------------------- AsyncThunk -------------------------------------
+
+export const login = createAsyncThunk("auth/login", async (form) => {
+  const payload = {
+    path: '/login',
+    requestBody: form,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  }
+  const response = await callApi(payload);
+  return response;
+});
+
+// ------------------------------------- Slice -------------------------------------
+
+const initialState = {
+  authEntity: {
+    username: null,
+    jwttoken: null
+  },
+  status: 'idle',
+  error: null
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout() {
+      return initialState;
+    }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(login.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.authEntity.username = action.payload.username;
+        state.authEntity.jwttoken = action.payload.jwttoken;
+        if (!state.authEntity.username) state.status = 'failed';
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
+});
+
+export const { logout } = authSlice.actions;
+
+// ------------------------------------- Selector -------------------------------------
+export const authSelector = state => state.auth;
+
+export default authSlice.reducer;
