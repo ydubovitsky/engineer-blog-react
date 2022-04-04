01bbf7e230eba51b93609e1dc91ba3f35454f8e6
user <y.dubovitsky@gmail.com>
2022-04-04 15:14:06 +0300

    RecPosts component added

---
+++ src/components/main/post-current/rec-posts/rec-posts.component.jsx
@@ -0,0 +1,26 @@
+import styles from './rec-posts.module.css';
+import { useSelector } from 'react-redux';
+import { postEntityByIdSelector } from '../../../../redux/features/post/postSlice';
+import ByteImage from '../../../common/hoc/byte-image/byte-image-component';
+
+const RecPosts = ({ id }) => {
+
+  const previousPost = useSelector(state => postEntityByIdSelector(state, parseInt(id - 1)));
+  const nextPost = useSelector(state => postEntityByIdSelector(state, parseInt(id + 1)));
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Recommended Posts</div>
+      <div className={styles.previousPost}>
+        <p>{previousPost?.title}</p>
+        <ByteImage byteImage={previousPost?.postImage} />
+      </div>
+      <div className={styles.nextPost}>
+        <p>{previousPost?.title}</p>
+        <ByteImage byteImage={nextPost?.postImage} />
+      </div>
+    </div>
+  )
+}
+
+export default RecPosts;
