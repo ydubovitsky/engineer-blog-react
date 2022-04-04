c12bef2110374f1fd910798e1380c85ce44f92bb
user <y.dubovitsky@gmail.com>
2022-04-04 13:24:53 +0300

    Added wrapper component over img, which takes byteImage property

---
+++ src/components/common/hoc/byte-image/byte-image-component.jsx
@@ -0,0 +1,12 @@
+import styles from './byte-image.module.css';
+import BlankImage from '../../../../resources/images/blank-image.jpg';
+
+const ByteImage = ({ byteImage }) => {
+  return (
+    <div className={styles.container}>
+      <img src={byteImage ? `data:image/png;base64,${byteImage}` : <BlankImage />} alt="" />
+    </div>
+  )
+}
+
+export default ByteImage;
