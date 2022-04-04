11ab807bbacd8a34f02489a6ffbd09757e36f196
user <y.dubovitsky@gmail.com>
2022-04-04 15:33:15 +0300

    Feature to save/load user and token to/from localStorage via store.subscribe and preloadState added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
-
+import { configureStore } from '@reduxjs/toolkit';
+import { loadState, saveState } from './localStorage';
 import authReducer from './features/auth/authSlice';
 import postReducer from './features/post/postSlice';
 
-export default configureStore({
+const persistedState = loadState('auth');
+
+const store = configureStore({
   reducer: {
     auth: authReducer,
     post: postReducer,
+  },
+  preloadedState: {
+    auth: persistedState
   }
-})
+});
+
+//TODO Доработать, чтобы можно было передавать объект с множеством полей!
+//! Save auth state
+store.subscribe(() => {
+  saveState('auth', store.getState().auth);
+});
+
+export default store;
