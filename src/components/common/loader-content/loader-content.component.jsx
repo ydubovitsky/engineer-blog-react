3e192cb5e39ed8d0f19473e3fbd89cd9fd39573d
user <y.dubovitsky@gmail.com>
2022-04-07 17:53:49 +0300

    Loader and ContentLoader added

---
+++ src/components/common/loader-content/loader-content.component.jsx
@@ -0,0 +1,10 @@
+import styles from './loader-content.module.css';
+
+const LoaderContent = ({style}) => {
+  return (
+    <div className={styles.container} style={style}>
+    </div>
+  )
+}
+
+export default LoaderContent;
---
+++ src/components/common/loader/loader.component.jsx
@@ -0,0 +1,14 @@
+import styles from './loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.container}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loader;
