af937e43a6b634f22f4f4955417908c1c204a9d7
user <y.dubovitsky@gmail.com>
2022-04-08 16:13:00 +0300

    Added simple navigation to dashboard

--- src/components/dashboard/navbar/navbar.component.jsx
+++ src/components/dashboard/navbar/navbar.component.jsx
@@ -1,9 +1,13 @@
 import styles from './navbar.module.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <p><i class="fas fa-user-shield"></i> Dashboard</p>
+      <Link to={"/dashboard/post-list"}>Post List</Link>
+      <Link to={"/main"}>Main</Link>
+      <Link to={"/main"}>Logout</Link>
     </div>
   )
 }
