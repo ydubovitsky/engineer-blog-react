996843e6788996cfae450eec0dfb92edbcdbf747
user <y.dubovitsky@gmail.com>
2022-04-20 11:53:47 +0300

    The Sidebar added

--- src/components/main/social-nav-panel/social-nav-panel.component.jsx
+++ src/components/main/social-nav-panel/social-nav-panel.component.jsx
@@ -1,8 +1,9 @@
+import cn from "classnames";
 import { Link } from "react-router-dom";
 import styles from './social-nav-panel.module.css';
 import { authSelector, logout } from '../../../redux/features/auth/authSlice';
 import { useSelector, useDispatch } from 'react-redux';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { LangContext } from '../../../context/LangContext';
 import LangSwitcher from '../lang-switcher/lang-switcher.component';
 
@@ -13,6 +14,7 @@ const SocialNavPanel = () => {
   //Context
   const { getLangData } = useContext(LangContext);
   const { socialNavPanel } = getLangData();
+  const [isSidebarShow, setIsSidebarShow] = useState(false);
 
   const showAuthButton = (status) => {
     if (status === 'succeeded') {
@@ -23,33 +25,35 @@ const SocialNavPanel = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.menu}>
-        <div className={styles.menuItem}><p>{socialNavPanel.menu.home}</p></div>
-        <div className={styles.menuItem}><p>{socialNavPanel.menu.about}</p></div>
-        <div className={styles.menuItem}><p>{socialNavPanel.menu.contacts}</p></div>
-        <div className={styles.menuItem}><p>{socialNavPanel.menu.policy}</p></div>
+      <div className={styles.sidebar} onClick={() => setIsSidebarShow(!isSidebarShow)}>
+        <i className="fa fa-bars"></i>
       </div>
-      <div className={styles.langSwitcher}>
-        <LangSwitcher />
+      <div className={cn(styles.navContainer, isSidebarShow ? styles.showSidebar : '')}>
+        <div className={styles.menu}>
+          <div className={styles.menuItem}><p>{socialNavPanel.menu.home}</p></div>
+          <div className={styles.menuItem}><p>{socialNavPanel.menu.about}</p></div>
+          <div className={styles.menuItem}><p>{socialNavPanel.menu.contacts}</p></div>
+          <div className={styles.menuItem}><p>{socialNavPanel.menu.policy}</p></div>
+        </div>
+        <div className={styles.langSwitcher}>
+          <LangSwitcher />
+        </div>
+        <div className={styles.socialContainer}>
+          <div className={styles.link}>
+            <i className="fab fa-facebook"></i>
+          </div>
+          <div className={styles.link}>
+            <i className="fab fa-twitter"></i>
+          </div>
+          <div className={styles.link}>
+            <i className="fab fa-google"></i>
+          </div>
+          <div className={styles.link}>
+            <i className="fab fa-github"></i>
+          </div>
+        </div>
+        {showAuthButton(status)}
       </div>
-      <div className={styles.socialContainer}>
-        <div className={styles.link}>
-          <i className="fab fa-facebook"></i>
-        </div>
-        <div className={styles.link}>
-          <i className="fab fa-twitter"></i>
-        </div>
-        <div className={styles.link}>
-          <i className="fab fa-google"></i>
-        </div>
-        <div className={styles.link}>
-          <i className="fab fa-google"></i>
-        </div>
-        <div className={styles.link}>
-          <i className="fab fa-github"></i>
-        </div>
-      </div>
-      {showAuthButton(status)}
     </div>
   )
 }
