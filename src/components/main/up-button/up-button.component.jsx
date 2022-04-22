853023d48e27ae2ba768653dcf16f1dd90a0e2dd
user <y.dubovitsky@gmail.com>
2022-04-22 14:44:25 +0300

    The arg fixed in the function - smoothScrollToElement in the UpButton component

--- src/components/main/up-button/up-button.component.jsx
+++ src/components/main/up-button/up-button.component.jsx
@@ -4,7 +4,7 @@ import { smoothScrollToElement } from '../../../utils/smoothScrollToElement';
 const UpButton = () => {
 
   return (
-    <div className={styles.container} onClick={() => smoothScrollToElement('content')}>
+    <div className={styles.container} onClick={() => smoothScrollToElement('container')}>
       <i className="fa fa-arrow-up"></i>
     </div>
   )
