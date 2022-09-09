7ee8b77de6ec584671a7a7ff8ff0ee3e77c56701
user <y.dubovitsky@gmail.com>
2022-09-09 14:39:40 +0300

    styles for post-list-item improved a bit

--- src/components/common/hoc/byte-image/byte-image-component.jsx
+++ src/components/common/hoc/byte-image/byte-image-component.jsx
@@ -1,10 +1,10 @@
 import styles from './byte-image.module.css';
-import BlankImage from '../../../../resources/images/blank-image.jpg';
+import BlankImage from './blank-image.jpg';
 
-const ByteImage = ({ byteImage = ''}) => { //TODO Добавить изображение по умолчанию?
+const ByteImage = ({ byteImage }) => { //TODO Добавить изображение по умолчанию?
   return (
     <div className={styles.container}>
-      <img src={byteImage ? `data:image/png;base64,${byteImage}` : <BlankImage />} alt="" />
+      <img src={byteImage ? `data:image/png;base64,${byteImage}` : BlankImage} alt="" />
     </div>
   )
 }
--- src/components/main/post-list-item/post-list-item.component.jsx
+++ src/components/main/post-list-item/post-list-item.component.jsx
@@ -15,11 +15,13 @@ const PostListItem = ({ id, postImage, category, title, createAt, description })
   return (
     <div className={styles.container}>
       <ByteImage byteImage={postImage.byteImage} />
-      <div className={styles.category}><span>Category - </span> {category}</div>
-      <div className={styles.title}>{title}</div>
-      <div className={styles.date}><span>Created at </span>{createAt}</div>
-      <div className={styles.description}>
-        {description}
+      <div className={styles.infoContainer}>
+        <div className={styles.category}><span>Category - </span> {category}</div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.date}><span>Created at </span>{createAt}</div>
+        <div className={styles.description}>
+          {description}
+        </div>
       </div>
       <Link to={`/main/posts/post?id=${id}`}><button className={styles.button}>{postListItem.button}</button></Link>
     </div>
