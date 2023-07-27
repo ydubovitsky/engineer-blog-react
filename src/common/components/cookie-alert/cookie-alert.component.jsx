e292fff6245c1399e459eab671c9d7d3c55c97b8
user <y.dubovitsky@gmail.com>
2023-07-27 15:31:27 +0300

    Cookie component updated; buttons added

--- src/common/components/cookie-alert/cookie-alert.component.jsx
+++ src/common/components/cookie-alert/cookie-alert.component.jsx
@@ -1,26 +1,31 @@
-import cn from 'classnames';
-import { useContext } from 'react';
-import { LangContext } from '../../../context/lang/LangContext';
+import { useContext, useRef } from 'react';
 import { cookieWrapper } from '../../../context/cookie';
+import { LangContext } from '../../../context/lang/LangContext';
+import ButtonComponent from '../../atomic-components/button/button.component';
 import styles from './cookie-alert.module.css';
 
-const CookieAlert = (props) => {
+const CookieAlert = () => {
 
   const { getLangData } = useContext(LangContext);
   const { cookieAlert } = getLangData();
-  const { cookies, setCookie } = props;
+  const containerRef = useRef();
 
-  if (cookies['agreeCookie'] === 'true') {
-    return null;
+  const stayHere = () => {
+    containerRef.current.classList.add(styles.hide)
+  }
+  
+  const redirect = () => {
+    window.location.href = 'https://google.com';
   }
 
   return (
-      <div className={styles.container}>
-        <p>{cookieAlert.text}</p>
-        <i onClick={() => setCookie('agreeCookie', 'true')}
-          className={cn(styles.closeBtn, "fas fa-times-circle")}
-        ></i>
+    <div className={styles.container} ref={containerRef}>
+      <p className={styles.text}>{cookieAlert.text}</p>
+      <div className={styles.buttonsContainer}>
+        <ButtonComponent name={cookieAlert.agree} clickFunction={stayHere} />
+        <ButtonComponent name={cookieAlert.disagree} clickFunction={redirect} />
       </div>
+    </div>
   )
 }
 
