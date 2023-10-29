9bf6eea500a36ed108feecad8397b8be9e37e09b
y-dubovitsky <pXmJ5M9dvP>
2023-10-29 17:44:15 +0300

    The slider inside Hero component redesigned

--- src/layouts/main/components/hero/hero.component.jsx
+++ src/layouts/main/components/hero/hero.component.jsx
@@ -1,38 +1,56 @@
-import cn from 'classnames';
-import styles from './hero.module.css';
-import { useContext, useState } from 'react';
-import { LangContext } from '../../../../context/lang/LangContext';
-import { smoothScrollToElement } from '../../../../utils/scroll.util';
+import cn from "classnames";
+import styles from "./hero.module.css";
+import { useContext, useState } from "react";
+import { LangContext } from "../../../../context/lang/LangContext";
+import { smoothScrollToElement } from "../../../../utils/scroll.util";
 //TODO! Сделать так, чтобы сразу массив импортировался!
-import {
-  FirstSlideImage,
-  SecondSlideImage,
-  ThirdSlideImage
-} from "./images";
+import { FirstSlideImage, SecondSlideImage, ThirdSlideImage } from "./images";
 
 const Hero = () => {
-
   const images = [FirstSlideImage, SecondSlideImage, ThirdSlideImage];
   const { getLangData } = useContext(LangContext);
   const { hero } = getLangData();
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  // Устанавливает рандомное число в диапазоне в качестве номера для текущего слайда
-  const sliderHandler = (_, min = 0, max = images.length - 1) => {
-    let rand = min + Math.random() * (max + 1 - min);
-    setCurrentSlide(Math.floor(rand));
-  }
+  const sliderHandler = (direction) => ()=> {
+    switch (direction) {
+      case "forward":
+        currentSlide < images.length - 1
+          ? setCurrentSlide((prev) => ++prev)
+          : setCurrentSlide(0);
+        break;
+      case "backward": {
+        currentSlide === 0
+          ? setCurrentSlide(images.length - 1)
+          : setCurrentSlide((prev) => --prev);
+        break;
+      }
+      default:
+        setCurrentSlide(0);
+    }
+  };
 
   const showDotsElements = () => {
     return images.map((_, idx) => {
-      return <div key={idx} className={cn(styles.dot, idx === currentSlide ? styles.dotActive : '')}></div>
-    })
-  }
+      return (
+        <div
+          key={idx}
+          className={cn(
+            styles.dot,
+            idx === currentSlide ? styles.dotActive : ""
+          )}
+        ></div>
+      );
+    });
+  };
 
   return (
-    <div className={styles.container} style={{ backgroundImage: `url(${images[currentSlide]})` }}>
+    <div
+      className={styles.container}
+      style={{ backgroundImage: `url(${images[currentSlide]})` }}
+    >
       <div className={styles.overlay}>
-        <div className={styles.right} onClick={sliderHandler}>
+        <div className={styles.right} onClick={sliderHandler("forward")}>
           <i className="fas fa-chevron-right"></i>
         </div>
         <div className={styles.sideHeader}>
@@ -42,14 +60,17 @@ const Hero = () => {
           <div className={styles.text}>
             <p>{hero.text}</p>
           </div>
-          <button className={styles.button} onClick={() => smoothScrollToElement('container')}>{hero.button}</button>
+          <button
+            className={styles.button}
+            onClick={() => smoothScrollToElement("container")}
+          >
+            {hero.button}
+          </button>
         </div>
-        <div className={styles.left} onClick={sliderHandler}>
+        <div className={styles.left} onClick={sliderHandler("backward")}>
           <i className="fas fa-chevron-left"></i>
         </div>
-        <div className={styles.pageDots}>
-          {showDotsElements()}
-        </div>
+        <div className={styles.pageDots}>{showDotsElements()}</div>
       </div>
     </div>
   );
